fd247f75405a9217d8f7f075a9d09f4b410edf96
daniyalhotelkey <daniyal.mateen@hotelkeyapp.com>
2022-06-29 16:29:49 +0500

    First Commit

---
+++ src/main/java/org/example/Class.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Class {
+    ArrayList<Student> stList;
+
+    Class()
+    {
+        stList=new ArrayList<Student>();
+    }
+
+    void addStudents()
+    {
+        try {
+
+            String fileName = "src/main/java/org/example/StudentsRecord.txt";
+            BufferedReader in = new BufferedReader(new FileReader(fileName));
+
+            String s;
+            while((s = in.readLine()) != null) {
+
+                String [] data= s.split(" ");
+                Student temp=new Student("",0,"");
+
+                temp.setName(data[0]);
+                temp.setAge(Integer.parseInt(data[1]));
+                temp.setRollNumber(data[2]);
+
+                stList.add(temp);
+
+
+            }
+            in.close();
+
+        } catch (IOException e) {
+
+            System.out.println("IOException:");
+            e.printStackTrace();
+
+        }
+
+
+    }
+
+
+}
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -3,15 +3,31 @@ package org.example;
 public class Student extends Person{
 
     String rollNumber;
+    int mathMarks;
+
+
 
     Student(String name,int age,String rollNumber)
     {
         super(name,age);
         this.rollNumber=rollNumber;
+        this.mathMarks=0;
     }
 
     String getRollNumber()
     {
         return rollNumber;
+    }
+
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public int getMathMarks() {
+        return mathMarks;
+    }
+
+    public void setMathMarks(int mathMarks) {
+        this.mathMarks = mathMarks;
     }
 }
